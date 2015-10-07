0b9765fa20c887d91a5d30bb19bfb234e1f2c60e
tsubasanoyume <tsubasaro@gmail.com>
2015-10-07 15:16:17 +0300

    Пятое задание. Параллакс

---
+++ js/game_demo.js
@@ -0,0 +1,33 @@
+(function() {
+
+  var HORIZONTAL_COORDINATE = 50;
+  var cloudsContainer = document.querySelector('.header-clouds');
+
+  function getCloudsScroll() {
+    return HORIZONTAL_COORDINATE - Math.ceil(window.scrollY/5);
+  }
+
+  function isContainerInTheWindow() {
+    return cloudsContainer.getBoundingClientRect().bottom > 0;
+  }
+
+  function checkContainerInTheWindow() {
+    if (isContainerInTheWindow()) {
+      window.dispatchEvent(new CustomEvent('offsetClouds'));
+    }
+  }
+
+  function initScroll() {
+    window.addEventListener('scroll', function() {
+      checkContainerInTheWindow();
+    });
+
+    window.addEventListener('offsetClouds', function() {
+      cloudsContainer.style.backgroundPosition = getCloudsScroll() + '%' + '0%';
+    })
+
+  }
+
+  initScroll();
+
+})();
